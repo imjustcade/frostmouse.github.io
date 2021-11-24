f9b8d2fca8909b503850c9c1b1057e06d09b0486
Cade Elmore <imjustcade@gmail.com>
2021-11-23 18:14:41 -0600

    removed angular logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Frost Mouse Studios, LLC';
+  title = 'Frost Mouse Studios';
+  title2 = 'Website coming soon'
 }
